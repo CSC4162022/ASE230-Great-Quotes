c0fefa8821ec93c50615bd228fd8c310d7e570e1
CD <98351932+CSC4162022@users.noreply.github.com>
2022-10-14 15:17:42 -0400

    Add files via upload

--- csv_util.php
+++ csv_util.php
@@ -1,148 +1,252 @@
 <?php
 
+class Utilities {
 
 
-//read the content of the csv file and return an array
-function getArrayFromCsv($fileName) {
-    return $arr = array_map('str_getcsv', file($fileName));
+    //append record to end of row or create new row
+    function addRecord($fileName, $authorIndex, $quote, $addNewAuthor) {
 
-}
+        $utilities = new Utilities();
+        $quotes = $utilities->getArrayFromCsv('../quotes.csv');
+        if ($addNewAuthor || $authorIndex > count($quotes)) {
+            // Fill the CSV file.
+            $file = fopen($fileName, 'w');
+            foreach ($quotes as $fields) {
+                fputcsv($file, $fields);
+            }
+            fclose($file);
 
-function getArrayElementFromCsv($fileName, $index) {
-    $arr = array_map('str_getcsv', file($fileName));
-    return $arr[$index];
-}
+            // Add a new line at the end of the file
+            $file = fopen($fileName, 'a');
+            fputcsv($file, [$quote]);
+            fclose($file);
+            return true;
 
-//append a record to the end of the row
-function addRecord($fileName, $authorIndex, $record) {
-    //get array of csv contents
-    $csvArray = getArrayFromCsv($fileName);
-
-    $i = 0;
-    $input = fopen($fileName, 'r');  //open for reading
-    $output = fopen('temporary.csv', 'w'); //open for writing
-
-    //get row contents using index of the author
-    while( false !== ( $data = fgetcsv($input) ) ){  //read line as array
-
-        //stop at selected author
-        if ($i == $authorIndex) {
-            array_push($data, $record);
-            fputcsv($output, $data);
-            $i++;
-            continue;
         }
-        else {
-            //skip the row
+        else  {
+            $input = fopen($fileName, 'r');  //open for reading
+            $output = fopen('../temporary.csv', 'w'); //open for writing
+            $i = 0;
+            //get row contents using index of the author
+            while( false !== ( $data = fgetcsv($input) ) ){  //read line as array
+
+                //stop at selected author
+                if ($i == $authorIndex) {
+                    array_push($data, $quote);
+                    fputcsv($output, $data);
+                    $i++;
+                    continue;
+                }
+                $i++;
+                //write modified data to new file
+                fputcsv( $output, $data);
+            }
+
+            fclose( $input );
+            fclose( $output );
+
+            //clean up
+            unlink($fileName);// Delete
+            rename('../temporary.csv', $fileName); //Rename
+            return true;
         }
-        //adding a new author
-        if($authorIndex > count($csvArray) && $i == $authorIndex - 1) {
-            fputcsv($output, $data);
-        }
-        $i++;
-        //write modified data to new file
-        fputcsv( $output, $data);
     }
 
-    fclose( $input );
-    fclose( $output );
+    function modifyRecord($fileName, $authorIndex, $recordIndex, $record) {
+        //get array of csv contents
+        $utilities = new Utilities();
+        $csvArray = $utilities->getArrayFromCsv($fileName);
+        $size = count($csvArray);
+        $i = 0;
+        $input = fopen($fileName, 'r');  //open for reading
+        $output = fopen('../temporary.csv', 'w'); //open for writing
 
-    //clean up
-    unlink($fileName);// Delete
-    rename('temporary.csv', $fileName); //Rename
-}
+        //get row contents using index of the author
+        while( false !== ( $data = fgetcsv($input) ) ){  //read line as array
+            //stop at selected author
+            if ($i == $authorIndex) {
+                for ($j=0;$j<count($data);$j++) {
+                    if ($j == $recordIndex) {
+                        $elem[] = $record;
+                        $data[$j] = $elem;
+                        fputcsv($output, $data);
+                    }
+                }
+                $i++;
+                continue;
+            }
+            $i++;
+            //write modified data to new file
+            fputcsv( $output, $data);
+        }
+        fclose( $input );
+        fclose( $output );
+        //clean up
+        unlink($fileName);// Delete
+        rename('../temporary.csv', $fileName); //Rename
+        return true;
+    }
 
-function modifyRecord($fileName, $authorIndex, $quoteIndex, $record) {
-    //get array of csv contents
-    $csvArray = getArrayFromCsv($fileName);
-    $i = 0;
-    $input = fopen($fileName, 'r');  //open for reading
-    $output = fopen('temporary.csv', 'w'); //open for writing
 
-    //get row contents using index of the author
-    while( false !== ( $data = fgetcsv($input) ) ){  //read line as array
-        //stop at selected author
-        if ($i == $authorIndex) {
-            for ($j=0;$j<count($data);$j++) {
-                if ($j == $quoteIndex) {
-                    $data[$j] = $record;
+
+    //Per instructions, a function to remove the contents of the entire row
+    function deleteRowFromCsv($fileName, $authorIndex) {
+
+        try {
+            $i = 0;
+            $input = fopen($fileName, 'r');  //open for reading
+            $output = fopen('../temporary.csv', 'w'); //open for writing
+
+            //get row contents using index of the author
+            while( false !== ( $data = fgetcsv($input) ) ){  //read line as array
+                //stop at selected author
+                if ($i == $authorIndex)
+                {
+                    $i++;
+                    continue;
+                    //skip the row
+                }
+                else {
+                    $i++;
                     fputcsv($output, $data);
                 }
             }
-            $i++;
-            continue;
+            fclose( $input );
+            fclose( $output );
+            //clean up
+            unlink($fileName);// Delete
+            rename('../temporary.csv', $fileName); //Rename
+            return true;
+        } catch (Exception $ex) {
+            die($ex . ' ' . ' deleting row from ' . $fileName);
         }
-        $i++;
-        //write modified data to new file
-        fputcsv( $output, $data);
     }
-    fclose( $input );
-    fclose( $output );
-    //clean up
-    unlink($fileName);// Delete
-    rename('temporary.csv', $fileName); //Rename
-    return true;
-}
 
-//delete the single entry
-function deleteStringFromCsv($quote, $quoteIndex, $authorIndex, $fileName) {
 
-    //get array of csv contents
-    $csvArray = getArrayFromCsv($fileName);
-    $i = 0;
-    $input = fopen($fileName, 'r');  //open for reading
-    $output = fopen('temporary.csv', 'w'); //open for writing
-
-    //get row contents using index of the author
-    while( false !== ( $data = fgetcsv($input) ) ){  //read line as array
-        //stop at selected author
-        if ($i == $authorIndex) {
-            for ($j=0;$j<count($data);$j++) {
-                if ($j == $quoteIndex) {
-                    $data[$j] = '';
-                    fputcsv($output, $data);
-                }
-            }
-            $i++;
-            continue;
+    //validate the email and password
+    function validateSigninInput($email, $password) {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return false;
         }
-        $i++;
-        //write modified data to new file
-        fputcsv( $output, $data);
-    }
-    fclose( $input );
-    fclose( $output );
-    //clean up
-    unlink($fileName);// Delete
-    rename('temporary.csv', $fileName); //Rename
-    return true;
-}
-
-//Per instructions, a function to remove the contents of the entire row
-function deleteRowFromCsv($fileName, $authorIndex) {
-
-    $i = 0;
-    $input = fopen($fileName, 'r');  //open for reading
-    $output = fopen('temporary.csv', 'w'); //open for writing
-
-    //get row contents using index of the author
-    while( false !== ( $data = fgetcsv($fileName) ) ){  //read line as array
-        //stop at selected author
-        if ($i == $authorIndex)
+        //password doesn't contain at least 2 symbols or isn't long enough
+        else if (!preg_match('/[\'^£$%&*!()}{@#~?><>,|=_+¬-]/', $password) || strlen($password) < 8)
         {
-            //skip the row
+            return false;
+        }
+        return true;
+    }
+
+    //check if the file containing banned users exists, check if the email has been banned
+    //check if the file containing users exists, check if the email is registered
+    function verifyUser($usersFile, $bannedUsersFile, $email) {
+        $emailMatch = false;
+        if(file_exists($usersFile) && file_exists($bannedUsersFile)) {
+
+            $usersLine = fopen($usersFile, 'r');  //open for reading
+            while( false !== ( $data = fgetcsv($usersLine) ) ) {
+                if($data[0] == $email) {
+                    $emailMatch = true;
+                }
+            }
+            if (userIsBanned($email) == true) {
+                die('You are banned');
+            }
+            if ($emailMatch == true) {
+                return true;
+            }
         }
         else {
-            fputcsv($output, $data);
+            return false;
         }
-        $i++;
-        fputcsv( $output, $data);
+
     }
-    fclose( $input );
-    fclose( $output );
-    //clean up
-    unlink($fileName);// Delete
-    rename('temporary.csv', $fileName); //Rename
+
+    function verifyPassword($password, $email, $usersFile) {
+
+        $usersLine = fopen($usersFile, 'r');  //open for reading
+        while( false !== ( $data = fgetcsv($usersLine) ) ) {
+            if($data[0] == $email) {
+                $verify = password_verify($password, $data[1]);
+                if ($verify) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //delete the single string
+    function deleteStringFromCsv($quote, $quoteIndex, $authorIndex, $fileName) {
+
+        //get array of csv contents
+        $csvArray = $this->getArrayFromCsv($fileName);
+        $i = 0;
+        $input = fopen($fileName, 'r');  //open for reading
+        $output = fopen('../temporary.csv', 'w'); //open for writing
+
+        //get row contents using index of the author
+        while( false !== ( $data = fgetcsv($input) ) ){  //read line as array
+            //stop at selected author
+            if ($i == $authorIndex) {
+                $temp = $data;
+                for ($j=0;$j<count($data);$j++) {
+                    if ($j == $quoteIndex) {
+                        unset($temp[$quoteIndex]);
+                        fputcsv($output, $temp);
+                    }
+                }
+                $i++;
+                continue;
+            }
+            $i++;
+            //write modified data to new file
+            fputcsv( $output, $data);
+        }
+        fclose( $input );
+        fclose( $output );
+        //clean up
+        unlink($fileName);// Delete
+        rename('../temporary.csv', $fileName); //Rename
+        return true;
+    }
+
+    //read the content of the csv file and return an array
+    function getArrayFromCsv($fileName) {
+        return $arr = array_map('str_getcsv', file($fileName));
+
+    }
+
+    function getArrayElementFromCsv($fileName, $index) {
+        $arr = array_map('str_getcsv', file($fileName));
+        return $arr[$index];
+    }
+
+    function emailExists($email, $usersFile) {
+
+        $usersLine = fopen($usersFile, 'r');  //open for reading
+        while( false !== ( $data = fgetcsv($usersLine) ) ) {
+            if($data[0] == $email) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    function encryptPassword($password) {
+        return password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    function userIsBanned($email)
+    {
+        $bannedUsersLine = fopen('../data/banned.csv', 'r');
+        while (false !== ($data = fgetcsv($bannedUsersLine))) {
+            if ($data[0] == $email) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,43 +1,59 @@
 <?php
 require "csv_util.php";
 
+$utilities = new Utilities();
+
 function printAuthorsQuotes($authors, $quotes) {
 
-    for($i=0; $i<count($authors); $i++){
-        ?>
-        <h3><?= $authors[$i][0] ?> <?= $authors[$i][1] ?></h3>
-        <?php
-        for($j=0; $j<count($quotes[$i]); $j++) {
-            if ($quotes[$i][$j]) {
+    try {
+        for($i=0; $i<count($authors); $i++){
+            if ($authors[$i][0] && $authors[$i][1]) {
                 ?>
-                <p><?= $quotes[$i][$j] ?><a href="detail.php?index=<?=$i?>&quote=<?=$j?>"><?= 'Detail' ?></a></p><br>
+                <p><strong><?= $authors[$i][0] ?> <?= $authors[$i][1] ?></strong></p>
                 <?php
             }
+            for($j=0; $j<count($quotes[$i]); $j++) {
+                if ($quotes[$i][$j]) {
+                    ?>
+                    <p><?= $quotes[$i][$j] ?><a href="quotes/detail.php?index=<?=$i?>&quote=<?=$j?>"><?= 'Quote Detail' ?></a></p>
+                    <?php
+                }
+            }
         }
+    } catch (Exception $ex) {
+        die('Exception: ' . $ex);
     }
+
 }
 
 ?>
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-        <link rel="stylesheet" href="assets/css/index.css" />
-        <title><?= 'Great Quotes' ?></title>
-    </head>
-    <body>
-        <div class="class="container-fluid">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/css/index.css" />
+    <title><?= 'Midterm' ?></title>
+</head>
+<body>
+<form class="">
+    <div class="container text-center">
+        <div class="form-outline mb-4">
             <?php
-    printAuthorsQuotes(getArrayFromCsv('authors.csv'),getArrayFromCsv('quotes.csv'));
-    ?>
-        <p><a href="create.php?"><?= 'Create Quote' ?></a></p>
+            printAuthorsQuotes($utilities->getArrayFromCsv('./authors.csv'),$utilities->getArrayFromCsv('./quotes.csv'));
+            ?>
+            <p><a href="quotes/create.php?"><?= 'Create Quote' ?></a></p>
+            <?php
+            ?>
         </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    </body>
+    </div>
+</form>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
 </html>
 
 
